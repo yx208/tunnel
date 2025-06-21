d439f6e590cc72a5957f5efcf0c2144361d563b2
Hawthorn <yx208.lin@gmail.com>
2025-06-21 18:46:46 +0800

    feat: speed-5

--- src/tus/progress_stream.rs
+++ src/tus/progress_stream.rs
@@ -2,7 +2,6 @@ use std::pin::Pin;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicU64, Ordering};
 use std::task::{Context, Poll};
-use std::time::{Duration, Instant};
 use bytes::Bytes;
 use futures::Stream;
 use pin_project_lite::pin_project;
