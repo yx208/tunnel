45365b784ffa3d52ab102b3158fb4e3ba0975961
Hawthorn <yx208.lin@gmail.com>
2025-08-27 15:41:17 +0800

    chore: dev

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -2,13 +2,11 @@ mod types;
 mod errors;
 mod traits;
 pub mod scheduler;
-mod tunnel;
 mod task;
 
 pub use types::{TransferId, TransferConfig};
 pub use errors::{Result, TransferError};
 pub use types::*;
 pub use traits::*;
-pub use tunnel::Tunnel;
 pub use task::{TransferTask, TaskEventHandle, TaskQueue};
 
